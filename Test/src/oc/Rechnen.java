50a43a73d1afbb1883032f1040a03ec52e910fce
Corine Mbobda <Corine.Mbobda@opitz-consulting.com>
2019-05-15 13:23:06 +0200

    jenkins test

---
+++ Test/src/oc/Rechnen.java
@@ -0,0 +1,13 @@
+package oc;
+
+public class Rechnen {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x = 3;
+		int y = 2;
+		int result = x * y ;
+		System.out.println("The result is: " +result);
+	}
+
+}
